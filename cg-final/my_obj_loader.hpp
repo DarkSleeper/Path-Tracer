2111deb90360f5b8da04a1cde60c0511515e3a7a
Fu <793613936@qq.com>
2023-02-12 14:48:41 +0800

    init commit

---
+++ cg-final/my_obj_loader.hpp
@@ -0,0 +1,62 @@
+#pragma once
+#include <iostream>
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tool/tiny_obj_loader.h"
+
+void my_load_obj(std::string const& filename, std::string const& basepath, 
+					std::vector<float>& vertices, std::vector<float>& normals, std::vector<float>& texcoord) {
+	auto load_model = [](tinyobj::attrib_t &attrib, std::vector<tinyobj::shape_t> &shapes, std::vector<tinyobj::material_t> &materials,
+						 const char* filename, const char* basepath = NULL, bool triangulate = true) -> bool {
+		std::string warn;
+		std::string err;
+		bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filename, basepath, triangulate);
+		if (!warn.empty()) std::cout << "WARN: " << warn << std::endl;
+		if (!err.empty()) std::cerr << "ERR: " << err << std::endl;
+		if (!ret) {
+			printf("Failed to load/parse .obj.\n");
+			return false;
+		}
+		return true;
+	};
+	tinyobj::attrib_t attrib;
+	std::vector<tinyobj::shape_t> shapes;
+	std::vector<tinyobj::material_t> materials;
+	bool triangulate = true;
+	load_model(attrib, shapes, materials, filename.data(), basepath.data(), triangulate);
+
+	// init
+	//int vertice_num = attrib.vertices.size() / 3;
+	//normals.resize(vertice_num * 3);
+
+	//for (size_t v = 0; v < vertice_num; v++) {
+	//	vertices.push_back(attrib.vertices[3 * v + 0]);
+	//	vertices.push_back(attrib.vertices[3 * v + 1]);
+	//	vertices.push_back(attrib.vertices[3 * v + 2]);
+	//}
+
+	// For each shape
+	for (size_t i = 0; i < shapes.size(); i++) {
+		size_t index_offset = 0;
+
+		// For each face
+		for (size_t f = 0; f < shapes[i].mesh.num_face_vertices.size(); f++) {
+			size_t fnum = shapes[i].mesh.num_face_vertices[f];
+
+			// For each vertex in the face
+			for (size_t v = 0; v < fnum; v++) {
+				tinyobj::index_t idx = shapes[i].mesh.indices[index_offset + v];
+				//indices.push_back(idx.vertex_index);
+				vertices.push_back(attrib.vertices[3 * idx.vertex_index + 0]);
+				vertices.push_back(attrib.vertices[3 * idx.vertex_index + 1]);
+				vertices.push_back(attrib.vertices[3 * idx.vertex_index + 2]);
+				normals.push_back(attrib.normals[3 * idx.normal_index + 0]);
+				normals.push_back(attrib.normals[3 * idx.normal_index + 1]);
+				normals.push_back(attrib.normals[3 * idx.normal_index + 2]);
+				texcoord.push_back(attrib.texcoords[2 * idx.texcoord_index + 0]);
+				texcoord.push_back(attrib.texcoords[2 * idx.texcoord_index + 1]);
+			}
+			index_offset += fnum;
+		}
+	}
+}
