044c0c6fcc2bd27ffb957b57fe4b1614558da3e6
Fu Renhong <793613936@qq.com>
2023-02-26 12:00:35 +0800

    add triangle input

--- cg-final/cg-final.cpp
+++ cg-final/cg-final.cpp
@@ -5,15 +5,11 @@
 
 int main()
 {
-    std::vector<float> vertices; 
-    std::vector<float> normals; 
-    std::vector<float> texcoord;
-    std::vector<int> mat_idx; 
     std::vector<Triangle> triangles;
     std::vector<Material> materials;
     std::string filename = "runtime/scene/cornell-box/cornell-box.obj";
     std::string basepath = "runtime/scene/cornell-box/";
-    my_load_obj(filename, basepath, vertices, normals, texcoord, mat_idx, materials);
+    my_load_obj(filename, basepath, triangles, materials);
 
     std::cout << "Hello World!\n";
 }
--- cg-final/my_obj_loader.hpp
+++ cg-final/my_obj_loader.hpp
@@ -6,9 +6,7 @@
 #define TINYOBJLOADER_IMPLEMENTATION
 #include "tool/tiny_obj_loader.h"
 
-void my_load_obj(std::string const& filename, std::string const& basepath, 
-					std::vector<float>& vertices, std::vector<float>& normals, std::vector<float>& texcoord,
-					std::vector<int>& mat_idx, std::vector<Material>& materials) {
+void my_load_obj(std::string const& filename, std::string const& basepath, std::vector<Triangle>& triangles, std::vector<Material>& materials) {
 	auto load_model = [](tinyobj::attrib_t &attrib, std::vector<tinyobj::shape_t> &shapes, std::vector<tinyobj::material_t> &materials,
 						 const char* filename, const char* basepath = NULL, bool triangulate = true) -> bool {
 		std::string warn;
@@ -46,23 +44,24 @@ void my_load_obj(std::string const& filename, std::string const& basepath,
 		// For each face
 		for (size_t f = 0; f < shapes[i].mesh.num_face_vertices.size(); f++) {
 			size_t fnum = shapes[i].mesh.num_face_vertices[f];
+			if (fnum != 3) printf("  face[%ld] is not triangle!\n", static_cast<long>(f));
 
+			Triangle tri;
 			// For each vertex in the face
 			for (size_t v = 0; v < fnum; v++) {
 				tinyobj::index_t idx = shapes[i].mesh.indices[index_offset + v];
 				//indices.push_back(idx.vertex_index);
-				vertices.push_back(attrib.vertices[3 * idx.vertex_index + 0]);
-				vertices.push_back(attrib.vertices[3 * idx.vertex_index + 1]);
-				vertices.push_back(attrib.vertices[3 * idx.vertex_index + 2]);
-				normals.push_back(attrib.normals[3 * idx.normal_index + 0]);
-				normals.push_back(attrib.normals[3 * idx.normal_index + 1]);
-				normals.push_back(attrib.normals[3 * idx.normal_index + 2]);
-				texcoord.push_back(attrib.texcoords[2 * idx.texcoord_index + 0]);
-				texcoord.push_back(attrib.texcoords[2 * idx.texcoord_index + 1]);
+				tri.position[v] = glm::vec3(attrib.vertices[3 * idx.vertex_index + 0],
+											attrib.vertices[3 * idx.vertex_index + 1],
+											attrib.vertices[3 * idx.vertex_index + 2]);
+				tri.normal[v]   = glm::vec3(attrib.normals[3 * idx.normal_index + 0],
+											attrib.normals[3 * idx.normal_index + 1],
+											attrib.normals[3 * idx.normal_index + 2]);
+				tri.texcoord[v] = glm::vec2(attrib.texcoords[2 * idx.texcoord_index + 0],
+											attrib.texcoords[2 * idx.texcoord_index + 1]);
 			}
-			mat_idx.push_back(shapes[i].mesh.material_ids[f]);
-
-			if (fnum != 3) printf("  face[%ld] is not triangle!\n", static_cast<long>(f));
+			tri.mat_idx = shapes[i].mesh.material_ids[f];
+			triangles.push_back(tri);
 
 			index_offset += fnum;
 		}
