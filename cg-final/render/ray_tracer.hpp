d21e6e7132e0470cdddb27a14ee941caf978e814
Fu Renhong <793613936@qq.com>
2023-03-07 18:37:36 +0800

    reorganize some codes and files

--- cg-final/render/ray_tracer.hpp
+++ cg-final/render/ray_tracer.hpp
@@ -43,7 +43,6 @@ public:
 			auto normal = hit.getNormal();
 			auto pin = hit.getIntersectionPoint();
 
-			//color = scene->getAmbientLight() * hit.getMaterial()->diffuse;
 			int num_lights = scene->getNumLights();
 
 			auto light_weight = rand() / float(RAND_MAX);
@@ -82,7 +81,6 @@ public:
 				auto rot_matrix = glm::mat3x3{ new_x,new_y,new_z };
 
 				next_dir = rot_matrix * next_dir;
-				//if (glm::dot(next_dir, new_z) < 0) std::cout << "nooo";
 			}
 
 			Ray next_ray(pin, next_dir);
@@ -95,10 +93,6 @@ public:
 					auto next_shade = Shade(next_ray, next_hit, bounces + 1, weight, indexOfRefraction);
 					color += (hit.getMaterial())->shade(ray, hit, next_dir, next_shade) * (2.f * M_PI);
 				}
-				/*else if (glm::dot(next_ray.getDirection(), next_hit.getNormal()) <= 0) {
-					auto next_shade = next_hit.getMaterial()->radiance;
-					color += (hit.getMaterial())->shade(ray, hit, next_dir, next_shade) * (2.f * M_PI);
-				}*/
 			} else {
 				color += scene->bg_color;
 			}
@@ -129,72 +123,6 @@ public:
 		if (intsec) {
 
 			color = Shade(ray, hit, bounces, weight, indexOfRefraction);
-
-			//auto normal = hit.getNormal();
-			//auto pin = hit.getIntersectionPoint();
-
-			//if (glm::dot(normal, ray.getDirection()) > 0 && shade_back == 1)
-			//	hit.set(hit.getT(), hit.getMaterial(), -1.f * normal, ray);
-
-			////color = scene->getAmbientLight() * hit.getMaterial()->diffuse;
-			//int num_lights = scene->getNumLights();
-			//// todo: change ray call
-
-			//auto light_weight = rand() / float(RAND_MAX);
-			//auto k = choose_light(light_weight);
-			//{
-			//	// compute light
-			//	glm::vec3 ldir, clit;
-			//	float Dis2Lit;
-			//	scene->getIllumination(k, pin, ldir, clit, Dis2Lit);
-			//	clit = clit / powf(Dis2Lit, 2) * scene->light_areas[k];
-
-			//	auto new_pin = pin;
-			//	if (shade_back) new_pin = new_pin + ldir * epsilon;//ÕâÀïÊÇepsilon
-			//	Ray ray2(new_pin, ldir);
-			//	Hit hit2(Dis2Lit, scene->bg_mat, glm::vec3(0, 0, 0));
-
-			//	if (shade_back) {
-			//		scene->group_intersect(ray2, hit2, 0);
-			//	} else {
-			//		scene->group_intersect(ray2, hit2, epsilon);
-			//	}
-			//	if (shadows == false) {
-			//		color += (hit.getMaterial())->shade(ray, hit, ldir, clit);
-			//	} else  if (hit2.getT() == INFINITY || posive(hit2.getT() - Dis2Lit) < 0.0001) {
-			//		color += (hit.getMaterial())->shade(ray, hit, ldir, clit);
-			//	}
-			//}
-
-			//if (!hit.getMaterial()->is_light) {
-			//	auto next_dir = glm::vec3();
-			//	{
-			//		auto r1 = rand() / float(RAND_MAX);
-			//		auto r2 = rand() / float(RAND_MAX);
-			//		next_dir = glm::vec3(sqrtf(1 - powf(r1, 2)) * cosf(2 * M_PI * r2), sqrtf(1 - powf(r1, 2)) * sinf(2 * M_PI * r2), r1);
-			//		//next_dir = glm::normalize(next_dir);
-
-			//		auto z = glm::vec3(0, 0, 1);
-			//		auto new_z = hit.getNormal();
-			//		if (posive(glm::dot(z, new_z)) >= 1 - epsilon) z = glm::vec3(0, 1, 0);
-			//		auto new_x = glm::cross(z, new_z);
-			//		new_x = glm::normalize(new_x);
-			//		auto new_y = glm::cross(new_z, new_x);
-			//		new_y = glm::normalize(new_y);
-			//		auto rot_matrix = glm::mat3x3{ new_x,new_y,new_z };
-
-			//		next_dir = rot_matrix * next_dir;
-			//		//if (glm::dot(next_dir, new_z) < 0) std::cout << "nooo";
-			//	}
-
-			//	Ray next_ray(pin, next_dir);
-			//	Hit next_hit(MAXnum, scene->bg_mat, glm::vec3(0, 0, 0));
-			//	auto next_shade = traceRay(next_ray, epsilon, bounces + 1, weight, indexOfRefraction, next_hit);
-
-			//	if (!next_hit.getMaterial()->is_light && next_hit.getT() != MAXnum) {
-			//		color += (hit.getMaterial())->shade(ray, hit, next_dir, next_shade) * (2.f * M_PI);
-			//	}
-			//}
 
 
 			//glm::vec3 reflectColor = hit.getMaterial()->getReflectiveColor();
